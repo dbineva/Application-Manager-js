361c9d59c9c3c852727c68dbc66f18881e50baff
dbineva <dianabinevabineva@gmail.com>
2020-07-06 22:16:18 +0300

    Fixed validation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let pAvailableToStart = "";
 let pSkills = "";
 let pPersonalPresentation = "";
 let pStudyFromHome = "";
-
+let requiredField = ""
 
 //hide and seek the add student form
 addBtn.addEventListener('click', () => {
@@ -38,35 +38,35 @@ function fetchStudents() {
 const studentCollection = document.getElementById('student-collection')
 
 function renderStudents(students) {
-    studentCollection.innerHTML = ""
-    students.forEach(function (student) {
-        console.log(student)
-        studentCollection.innerHTML += `
-    <div class="bg-dark border border-success card mb-2">
-    <div class="card-body">
-    <p> Name: ${student.name} </p>
-    <p> Email: ${student.email} </p>
-    <p> Age: ${student.age} </p>
-    <p> Phone: ${student.phone} </p>
-    <p> Way of communication: ${student.wayOfComunication} </p>
-    <p> English Level: ${student.englishLevel} </p>
-    <p> Available to start on: ${student.availableToStart} </p>
-    <p> Skills: ${student.skills} </p>
-    <p> Personal presentation: ${student.personalPresentation} </p>
-    <p> Study from home: ${student.studyFromHome} </p>
-    <button class="col-5 text-white btn btn-warning edit-btn" id="${student.id}" onclick=editItem() ><i class="fas fa-edit"></i> Edit</button>
-    <button class="col-5 text-white btn btn-danger delete-btn" id="${student.id}" onclick=deleteItem()><i class="fas fa-trash-alt"></i> Delete</button>
-    </div>
-    </div>
-    `
-    })
+        studentCollection.innerHTML = ""
+        students.forEach(function (student) {
+            console.log(student)
+            studentCollection.innerHTML += `
+        <div class="bg-dark border border-success card mb-2">
+        <div class="card-body">
+        <p> Name: ${student.name} </p>
+        <p> Email: ${student.email} </p>
+        <p> Age: ${student.age} </p>
+        <p> Phone: ${student.phone} </p>
+        <p> Way of communication: ${student.wayOfComunication} </p>
+        <p> English Level: ${student.englishLevel} </p>
+        <p> Available to start on: ${student.availableToStart} </p>
+        <p> Skills: ${student.skills} </p>
+        <p> Personal presentation: ${student.personalPresentation} </p>
+        <p> Study from home: ${student.studyFromHome} </p>
+        <button class="col-5 text-white btn btn-warning edit-btn" id="${student.id}" onclick=editItem() ><i class="fas fa-edit"></i> Edit</button>
+        <button class="col-5 text-white btn btn-danger delete-btn" id="${student.id}" onclick=deleteItem()><i class="fas fa-trash-alt"></i> Delete</button>
+        </div>
+        </div>
+        `
+        }) 
+    
+    
 }
 fetchStudents();
 
 
-
 const addStudentForm = document.querySelector('.add-student-form')
-
 
 addStudentForm.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -74,18 +74,19 @@ addStudentForm.addEventListener('submit', function (event) {
 
 
 $("#insert").click(function () {
-    pName = $("#name").val();
-    pEmail = $("#email").val();
-    pAge = $("#age").val();
-    pPhone = $("#phone").val();
-    pWayOfComunication = $("input[name=way]").val();
-    pEnglishLevel = $("#level").val();
-    pAvailableToStart = $("#start-date").val();
-    pSkills = $("#skills").val();
-    pPersonalPresentation = $("#reasons").val();
-    pStudyFromHome = $("#home-study:checked").val();
+     pName = $("#name").val();
+     pEmail = $("#email").val();
+     pAge = $("#age").val();
+     pPhone = $("#phone").val();
+     pWayOfComunication = $("input[name=way]").val();
+     pEnglishLevel = $("#level").val();
+     pAvailableToStart = $("#start-date").val();
+     pSkills = $("#skills").val();
+     pPersonalPresentation = $("#reasons").val();
+     pStudyFromHome = $("#home-study:checked").val();
 
-    fetch(`http://localhost:3000/students`, {
+     if(required(pName,pEmail,pAge,pPhone,pEnglishLevel,pAvailableToStart)){
+        fetch(`http://localhost:3000/students`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -104,43 +105,41 @@ $("#insert").click(function () {
             })
         })
         .then(resp => {
-            validateForm()
             const newStudent = resp.json()
+            if(newStudent.name == "opit"){
+                location.reload();
+            }
             return newStudent;
-        })
-        .then(newStudent => renderStudents(newStudent))
-
+        }).then((newStudent) => renderStudents(newStudent))
+     }
+    else{
+        error(requiredField)
+    }
 })
 
 //handle edit
-
 function editItem() {
     const itemId = event.srcElement.id;
     console.log("edit" + itemId);
     addStudentForm.addEventListener('submit', function (event) {
         event.preventDefault();
-
     });
     fetch(`http://localhost:3000/students/${itemId}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
-            })
-            .then(resp =>{
-               const student= resp.json()
-               return student
-            })
-
-            .then(student => editDataForm(student))
-                
-                       
-
+        })
+        .then(resp => {
+            const student = resp.json()
+            return student
+        })
+        .then(student => editDataForm(student))
 }
 
-function editDataForm(student){
+function editDataForm(student) {
     studentCollection.innerHTML = ""
-        studentCollection.innerHTML += `
+    studentCollection.innerHTML += `
     <div class="bg-dark border border-success card mb-2">
     <div class="card-body">
     <div> Name:<input type="text" class= "input-control mt-2" id="newName" value="${student.name}" required> </div>
@@ -151,8 +150,7 @@ function editDataForm(student){
     <input type="radio" class="ml-5 mt-3" id="email-radio" name="way" value="email" required>Email
      <input type="radio" class="ml-5 mt-3" id="phone-radio" name="way" value="phone" required>Phone
      </div>
-
-    <div class="mt-3"> English Level:  <select id="newLevel" class="form-control " name="englishLevel"  value= "${student.englishLevel}" required>
+ <div class="mt-3"> English Level:  <select id="newLevel" class="form-control " name="englishLevel"  value= "${student.englishLevel}" required>
     <option value="-">-</option>
     <option value="beginner">Beginner</option>
     <option value="elementary">Elementary </option>
@@ -161,8 +159,7 @@ function editDataForm(student){
     <option value="advanced">Advanced</option>
     <option value="proficiency">Proficiency</option>
 </select>
-    
-${student.englishLevel} </div>
+    </div>
     <div> Available to start on:<input type="date" class= "input-control mt-3" id="newDate" value="${student.availableToStart}" required> </div>
     <div class="mt-3"> Skills:</div> <textarea class= "input-control " id="newSkills" value="${student.skills}" cols="30" rows="5" ></textarea> 
     <div class="mt-3"> Personal presentation: </div> <textarea class= "input-control " id="newPresentation" value="${student.personalPresentation}" cols="30" rows="5" ></textarea> 
@@ -172,20 +169,20 @@ ${student.englishLevel} </div>
     </div>
     </div>
     `
-    
+
 }
 
-function updateData(){
-    nName = $("#newName").val();
-    nEmail = $("#newEmail").val();
-    nAge = $("#newAge").val();
-    nPhone = $("#newPhone").val();
-    nWayOfComunication = $("input[name=way]").val();
-    nEnglishLevel = $("#newLevel").val();
-    nAvailableToStart = $("#newDate").val();
-    nSkills = $("#newSkills").val();
-    nPersonalPresentation = $("#newPresentation").val();
-    nStudyFromHome = $("#home-study:checked").val();
+function updateData() {
+    let nName = $("#newName").val();
+    let nEmail = $("#newEmail").val();
+    let nAge = $("#newAge").val();
+    let nPhone = $("#newPhone").val();
+    let nWayOfComunication = $("input[name=way]").val();
+    let nEnglishLevel = $("#newLevel").val();
+    let nAvailableToStart = $("#newDate").val();
+    let nSkills = $("#newSkills").val();
+    let nPersonalPresentation = $("#newPresentation").val();
+    let nStudyFromHome = $("#home-study:checked").val();
     const itemId = event.srcElement.id;
     console.log("update" + itemId);
     addStudentForm.addEventListener('submit', function (event) {
@@ -210,18 +207,11 @@ function updateData(){
             })
         })
         .then(resp => {
-           
             const newStudent = resp.json()
             return newStudent;
         })
-      //  .then(newStudent => renderStudents(newStudent))
-      .then(newStudent => console.log(newStudent))
-
+        .then(newStudent => console.log(newStudent))
 }
-
-
-
-
 
 
 //handle delete
@@ -243,116 +233,81 @@ function deleteItem() {
 
 
 
-//form validation
-function validateForm() {
-    let vName = document.getElementById('name').value;
-    let vEmail = document.getElementById('email').value;
-    let vAge = document.getElementById('age').value;
-    let vPhone = document.getElementById('phone').value;
-    let vEnglishLevel = document.getElementById('level').value;
-    let vDate = document.getElementById('start-date').value;
-
-    let letters = /^[A-Za-z]+$/;
-    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let todaysDate = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
-
-
-
-
-    if (vName.length < 3 || vName == "" || !vName.match(letters)) {
-        error += "*Invalid name! Pleace enter your name!";
-        document.getElementById('form-error').innerHTML = error;
-        checkError(error);
-        error = "";
-        checkError(error);
-        return false;
-    }
-
-    if (!vEmail.match(regex)) {
-        error += "*Invalid Email!";
-        document.getElementById('form-error').innerHTML = error;
-        return false;
-    }
-    
-    if (vAge === "" || vAge === "0") {
-        error += "*Invalid age! Pleace enter your age!";
-        document.getElementById('form-error').innerHTML = error;
-        checkError(error);
-        error = "";
-        checkError(error);
-        return false;
-    }
-    //check about that one 
-    if (vPhone.length < 10) {
-        error += "*You have to enter a valid 10 digid nember!";
-        document.getElementById('form-error').innerHTML = error;
-        error = "";
-        return false;
-
-    }
-    //its not nessesery
-    if (!(Vcommunication[0].checked || Vcommunication[1].checked)) {
-        error += "*You have to check one of the options! ";
-        document.getElementById('form-error').innerHTML = error;
-        error = "";
-        return false;
-
-    }
-
-    if (vEnglishLevel === "-") {
-        error += "*You have to choose level of English!";
-        document.getElementById('form-error').innerHTML = error;
-        error = "";
-        return false;
-
-    }
-
-    // has to be after todays date
-    if (vDate == "" || vDate < todaysDate) {
-        error += "*You have to enter start date!";
-        document.getElementById('form-error').innerHTML = error;
-        error = "";
-        return false;
-    } else {
-        error += "*Your registration is successful!";
-        document.getElementById('form-error').innerHTML = error;
-        return true;
-    }
-}
-
-function checkError(err) {
-    switch (err) {
-        case "*Invalid name! Pleace enter your name!":
-            $("#name").css('border-color', 'red');
-            break;
-        case "*Invalid Email!":
-            $("#email").css('border-color', 'red');
-            break;
-        case "*Invalid age! Pleace enter your age!":
-            $("#age").css('border-color', 'red');
-            break;
-        case "*You have to enter a valid 10 digid nember!":
-            $("#phone").css('border-color', 'red');
-            break;
-        case "*You have to choose level of English!":
-            $("#level").css('border-color', 'red');
-            break;
-        case "*You have to enter start date!":
-            $("#start-date").css('border-color', 'red');
-            break;
-        case "":
-            resetStyle();
-            break;
-    }
-}
-
 function resetStyle() {
     $("#name").css('border-color', '#c5ecfd');
     $("#email").css('border-color', '#c5ecfd');
     $("#age").css('border-color', '#c5ecfd');
     $("#phone").css('border-color', '#c5ecfd');
-    $("#name").css('border-color', '#c5ecfd');
     $("#level").css('border-color', '#c5ecfd');
     $("#start-date").css('border-color', '#c5ecfd');
 
+}
+function error(err){
+    switch(err){
+        case"name":
+        $("#name").css('border-color', 'red');
+        break;
+
+        case"email":
+        $("#email").css('border-color', 'red');
+        break;
+
+        case"age":
+        $("#age").css('border-color', 'red');
+        break;
+
+        case"phone":
+        $("#phone").css('border-color', 'red');
+        break;
+
+        case"level":
+        $("#level").css('border-color', 'red');
+        break;
+
+        case"startDate":
+        $("#start-date").css('border-color', 'red');
+        break;
+    }
+}
+
+//form validation
+function required(name,email,age,phone,level,startDate) {
+    resetStyle();
+    //let letters = /^[A-Za-z]+$ && " "/ ;
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let todaysDate = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
+
+    if(name.length < 3 || name == "" ){
+       $("#form-error").html("*Invalid name! Pleace enter your name!").css("color","red");
+        requiredField = 'name'
+        return false
+    }
+    if(!email.match(regex)){
+        $("#form-error").html("*Invalid Email!").css("color","red");
+        requiredField = 'email'
+        return false;
+    }
+    if(age === "" ){
+        $("#form-error").html("*Invalid Age!").css("color","red");
+        requiredField = 'age'
+        return false
+    }
+    if(phone == ""){
+        $("#form-error").html("*Invalid Phone!").css("color","red");
+        requiredField = 'phone'
+        return false
+    }
+    if(level == "-"){
+        $("#form-error").html("*You have to choose level of English!").css("color","red");
+        requiredField = 'level'
+        return false
+    }
+    if(startDate == "" || startDate< todaysDate){
+        $("#form-error").html("*You have to enter correct start date!").css("color","red");
+        requiredField = 'startDate'
+        return false
+    }
+    else{
+        return true
+    }
 }
